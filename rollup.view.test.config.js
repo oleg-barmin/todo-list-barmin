b9932d52f8e7e1e660c20318ffa3739a68b8cdb5
Oleg <olegbarmin@gmail.com>
2018-09-05 14:50:17 +0300

    EventBus test was added.

---
+++ rollup.view.test.config.js
@@ -0,0 +1,7 @@
+export default {
+    input: 'test/view-test.js',
+    output: {
+        file: 'test/view-test-bundle.js',
+        format: 'umd',
+    },
+};
---
+++ test/view-test-bundle.js
@@ -0,0 +1,158 @@
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory() :
+    typeof define === 'function' && define.amd ? define(factory) :
+    (factory());
+}(this, (function () { 'use strict';
+
+    /**
+     * Transport object transfers data between {@link Controller} and TodoComponents.
+     *
+     * Is used in {@link EventBus}.
+     */
+    class Event {
+        /**
+         * Creates `Event` instance.
+         *
+         * @param {EventType} eventType type of this event
+         */
+        constructor(eventType) {
+            this.eventType = eventType;
+        }
+    }
+
+    /**
+     * Allows to posts {@link Event} and subscribe on `EventType` to process custom callbacks.
+     *
+     * When `Event` was posted all callbacks for `EventType` of occurred `Event` will be performed.
+     * For `EventBus` work properly transport JQuery object should be provided.
+     *
+     * Example:
+     * ```
+     * const firstCustomEventType = new EventType("firstCustomEventType");
+     * const secondCustomEventType = new EventType("secondCustomEventType");
+     * const eventBus = new EventBus(transport);
+     * ```
+     *
+     * When eventBus and custom EventTypes was declared you can bind callbacks for this event types.
+     *
+     * ```
+     * eventBus.subscribe(firstCustomEventType, function(occurredEvent){
+     *     console.log("First callback, occurredEvent: " + occurredEvent.eventType.typeName);
+     * });
+     *
+     * eventBus.subscribe(firstCustomEventType, function(occurredEvent){
+     *     console.log("Second callback, occurredEvent: " + occurredEvent.eventType.typeName);
+     * });
+     *
+     * eventBus.subscribe(secondCustomEventType, function(occurredEvent){
+     *     console.log("Third callback, occurredEvent: " + occurredEvent.eventType.typeName);
+     * });
+     * ```
+     *
+     * Now given callbacks will be performed, when `Event` with corresponded `EventType` will be occurred.
+     *
+     * ```
+     * //first and second callback will be performed
+     * eventBus.post(new Event(firstCustomEventType));
+     * ```
+     *
+     * Console output after post:
+     * <p>
+     * `
+     * First callback, occurredEvent: firstCustomEventType
+     * Second callback, occurredEvent: secondCustomEventType
+     * `
+     *
+     * if event of `secondCustomEventType` will be posted, only third callback will be processed.
+     * ```
+     * eventBus.post(new Event(secondCustomEventType));
+     * ```
+     *  Console output after post:
+     *  <p>
+     * `
+     * Third callback, occurredEvent: secondCustomEventType
+     * `
+     *
+     * Implementation of "event bus" design pattern, based on jquery.
+     */
+    class EventBus {
+
+        /**
+         * Creates `EventBus` instance.
+         *
+         * @param transport transport JQuery object to bind `EventBus` on.
+         */
+        constructor(transport) {
+            this._transport = transport;
+        }
+
+        /**
+         * Triggers all callback bound on `EventType` of given `Event`.
+         *
+         * @param {Event} event event which will be passed as argument to callbacks
+         *                which subscribed to the `EventType` of given event.
+         */
+        post(event) {
+            let typeName = event.eventType.typeName;
+            this._transport.trigger(typeName, [event]);
+        }
+
+        /**
+         * Binds given callback to desired `EventType`.
+         *
+         * @param {EventType} eventType `EventType` to which callback should be bound
+         * @param {Function} callback to bind onto `EventType`
+         */
+        subscribe(eventType, callback) {
+            this._transport.on(eventType.typeName, function (event, occurredEvent) {
+                callback(occurredEvent);
+            });
+        }
+    }
+
+    /**
+     * Marks type of {@link Event} to {@link EventBus} bind and call callback of specified `EventType`.
+     */
+    class EventType {
+        /**
+         * Creates `EventType` instance.
+         *
+         * @param {string} typeName string name of the `EventType` instance.
+         */
+        constructor(typeName) {
+            this.typeName = typeName;
+        }
+    }
+
+    QUnit.module("EventBus should");
+    QUnit.test("call ", assert => {
+        let transportElement = $("#eventBus");
+        const eventBus = new EventBus(transportElement);
+
+        const firstEventType = new EventType("firstEventType");
+        const secondEventType = new EventType("secondEventType");
+
+        const firstEvent = new Event(firstEventType);
+        const secondEvent = new Event(secondEventType);
+
+        firstEvent.interaction = 0;
+        secondEvent.interaction = 0;
+
+        const callback = occurredEvent => occurredEvent.interaction += 1;
+
+        eventBus.subscribe(firstEventType, callback);
+        eventBus.subscribe(secondEventType, callback);
+
+        eventBus.post(firstEvent);
+
+        assert.strictEqual(firstEvent.interaction, 1, "callback once for single post.");
+
+        eventBus.post(firstEvent);
+        eventBus.post(firstEvent);
+
+        assert.strictEqual(firstEvent.interaction, 3, "callback twice for two posts.");
+        assert.strictEqual(secondEvent.interaction, 0, "call only subscribed for this event callbacks.");
+
+    });
+
+})));
