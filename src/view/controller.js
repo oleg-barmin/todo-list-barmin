69bf5034b5eee2b7518ebd76f3761a1771213983
Oleg <olegbarmin@gmail.com>
2018-09-05 13:33:11 +0300

    Controller documentation was added.

--- src/view/controller.js
+++ src/view/controller.js
@@ -1,14 +1,24 @@
 import {TodoList} from "../model/todo-list";
-import {EventTypeEnum} from "./event/event";
+import {EventTypeEnumeration} from "./event/event";
 import {NewTaskAddedEvent} from "./event/newTaskAddedEvent";
 
+/**
+ * Connects model of To-do list - {@link TodoList} and To-do Components.
+ * Reacts on {@link Event} which occurred on view layer.
+ */
 export class Controller {
+
+    /**
+     * Creates `Controller` instance and subscribes on necessary event types.
+     *
+     * @param {EventBus} eventBus evenBus to work with.
+     */
     constructor(eventBus) {
         this.todoList = new TodoList();
         this.eventBus = eventBus;
 
         const self = this;
-        eventBus.subscribe(EventTypeEnum.AddTaskRequest, function (event) {
+        eventBus.subscribe(EventTypeEnumeration.AddTaskRequest, function (event) {
             self.todoList.add(event.data);
             self.eventBus.post(new NewTaskAddedEvent(self.todoList.all()))
         });
