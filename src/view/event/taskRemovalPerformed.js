eda1032ea86a17b2aa17c639f5baa12cf7f79a67
Oleg <olegbarmin@gmail.com>
2018-09-07 13:14:56 +0300

    TaskRemovalPerformed event was added.

---
+++ src/view/event/taskRemovalPerformed.js
@@ -0,0 +1,16 @@
+import {Event, EventTypes} from "./event";
+
+/**
+ * Occurs when processing of `TaskRemovalRequested` event was performed successfully.
+ */
+export class TaskRemovalPerformed extends Event{
+
+    /**
+     * Creates `TaskRemovalPerformed` instance.
+     * @param taskId ID of the task, which removal was performed
+     */
+    constructor(taskId){
+        super(EventTypes.TaskRemovalPerformed);
+        this.taskId = taskId;
+    }
+}
