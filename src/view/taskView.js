34496265bf9f1c87533af50db0b4c58a7f827986
olegbarmin <vTKX2CxE>
2018-09-08 18:14:26 +0300

    TaskView subscribed on TaskUpdatePerformed event. On TaskUpdatePerformed TaskView switch state to TaskDisplay

--- src/view/taskView.js
+++ src/view/taskView.js
@@ -10,10 +10,12 @@ import {TaskEdit} from "./taskEdit";
  *  - {@link TaskDisplay}
  *  - {@link TaskEdit}
  *
+ *
  *  In `TaskDisplay` state end user is able to:
  *  - mark task as completed
  *  - remove task
  *  - switch to `TaskEdit` state
+ *
  *
  *  In `TaskEdit` state end user is able to:
  *  - edit task description
@@ -27,26 +29,20 @@ export class TaskView extends TodoComponent {
     /**
      * Creates `TaskView` instance.
      *
-     * @param element Jquery element to render into
+     * @param {jQuery} element jQuery element to render into
      * @param {EventBus} eventBus eventBust to subscribe and post events
      * @param {Number} number number of the task in the list of tasks
      * @param {Task} task task to render
      */
     constructor(element, eventBus, number, task) {
         super(element, eventBus);
+
         this.eventBus = eventBus;
         this.task = task;
         this.number = number;
-        this.currentState = new TaskDisplay(this.element, this.eventBus, this.number, this.task);
-    }
+        this.currentState = new TaskDisplay(null, this.eventBus, null, null);
 
-    /**
-     * Renders given task in `TaskDisplay` state.
-     */
-    render() {
-        this.currentState.render();
-
-        const startTaskEditingHandler = this.eventBus.subscribe(EventTypes.StartTaskEditing, occurredEvent => {
+        const startTaskEditingHandler = this.eventBus.subscribe(EventTypes.TaskEditingStarted, occurredEvent => {
             if (occurredEvent.taskId === this.task.id) {
                 this.element.empty();
                 this.currentState = new TaskEdit(this.element, this.eventBus, this.number, this.task);
@@ -62,13 +58,42 @@ export class TaskView extends TodoComponent {
             }
         });
 
+        const taskUpdatePerformedHandler = this.eventBus.subscribe(EventTypes.TaskUpdatePerformed, occurredEvent => {
+            if (occurredEvent.taskId === this.task.id) {
+                this.element.empty();
+                this.currentState = new TaskDisplay(this.element, this.eventBus, this.number, this.task);
+                this.currentState.render();
+            }
+        });
+
         const taskRemovalPerformedHandler = this.eventBus.subscribe(EventTypes.TaskRemovalPerformed, (occurredEvent) => {
             if (occurredEvent.taskId === this.task.id) {
                 this.element.remove();
-                this.eventBus.unsubscribe(EventTypes.StartTaskEditing, startTaskEditingHandler);
+                this.eventBus.unsubscribe(EventTypes.TaskEditingStarted, startTaskEditingHandler);
                 this.eventBus.unsubscribe(EventTypes.CancelTaskEditing, cancelTaskEditingHandler);
                 this.eventBus.unsubscribe(EventTypes.TaskRemovalPerformed, taskRemovalPerformedHandler);
+                this.eventBus.unsubscribe(EventTypes.TaskUpdatePerformed, taskUpdatePerformedHandler);
             }
         });
+    }
+
+    /**
+     * Renders given task in `TaskDisplay` state onto given element.
+     * if method was called
+     *
+     * @param {jQuery} [element] jQuery element to render into
+     * @param {number} [number] number of the task in list
+     * @param {Task} [task] task to render.
+     */
+    render(element = undefined, number = undefined, task = undefined) {
+        this.element = element || this.element;
+        this.number = number || this.number;
+        this.task = task || this.task;
+
+        this.currentState.element = this.element;
+        this.currentState.number = this.number;
+        this.currentState.task = this.task;
+
+        this.currentState.render();
     }
 }
