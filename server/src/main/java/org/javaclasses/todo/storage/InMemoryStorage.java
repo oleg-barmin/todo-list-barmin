40b053508294f87a69210363087c1a17c43077a0
Oleg <olegbarmin@gmail.com>
2018-09-14 13:09:31 +0300

    Add InMemoryStorage abstract class.

---
+++ server/src/main/java/org/javaclasses/todo/storage/InMemoryStorage.java
@@ -0,0 +1,46 @@
+package org.javaclasses.todo.storage;
+
+import com.google.common.base.Preconditions;
+import org.javaclasses.todo.model.Entity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Stores Entities by their ID in memory.
+ *
+ * @param <I> ID of Entity
+ * @param <E> Entity to store
+ */
+public abstract class InMemoryStorage<I, E extends Entity<I>> implements Storage<I, E>{
+    private final Map<I, E> storage = new HashMap<>();
+
+    protected E create(E entity) {
+        Preconditions.checkNotNull(entity);
+        Preconditions.checkNotNull(entity.getId(), "To create Entity it must have not null ID.");
+
+        storage.put(entity.getId(), entity);
+
+        return entity;
+    }
+
+    protected E findById(I id) {
+        Preconditions.checkNotNull(id, "Cannot find entity with null ID.");
+
+        return storage.get(id);
+    }
+
+    protected void update(E entity) {
+        Preconditions.checkNotNull(entity);
+        Preconditions.checkNotNull(entity.getId());
+
+        storage.put(entity.getId(), entity);
+    }
+
+    @Override
+    public void remove(I id) {
+        Preconditions.checkNotNull(id, "Cannot remove entity with null ID.");
+
+        storage.remove(id);
+    }
+}
