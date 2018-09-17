92d27ceaacbc3e7d381967e6f9099992d6a6c79e
Oleg <olegbarmin@gmail.com>
2018-09-17 21:25:59 +0300

    Add getAllTaskOfTodoList() method to TaskStorage.

--- server/src/main/java/org/javaclasses/todo/storage/impl/TaskStorage.java
+++ server/src/main/java/org/javaclasses/todo/storage/impl/TaskStorage.java
@@ -2,10 +2,22 @@ package org.javaclasses.todo.storage.impl;
 
 import org.javaclasses.todo.model.Task;
 import org.javaclasses.todo.model.TaskId;
+import org.javaclasses.todo.model.TodoListId;
+
+import java.util.List;
 
 /**
  * Storage of `Task` entities by their `TaskId`.
  */
 public class TaskStorage extends InMemoryStorage<TaskId, Task> {
 
+    /**
+     * Provides list of tasks of `TodoList` with given ID.
+     *
+     * @param todoListId ID of `TodoList` which `Task`s required
+     * @return list of `Task`s of `TodoList`
+     */
+    public List<Task> getAllTaskOfTodoList(TodoListId todoListId) {
+        return findEntitiesWithField("todoListId", todoListId);
+    }
 }
