a89bb6971bf78081902d4d61f9312d4e492d4d99
Oleg <olegbarmin@gmail.com>
2018-09-24 20:16:27 +0300

    Refactor entity equals test to separated nested tests.

--- server/src/test/java/org/javaclasses/todo/model/EntityTest.java
+++ server/src/test/java/org/javaclasses/todo/model/EntityTest.java
@@ -2,69 +2,89 @@ package org.javaclasses.todo.model;
 
 import com.google.common.testing.EqualsTester;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import java.util.Date;
 import java.util.UUID;
 
 /**
  * @author Oleg Barmin
  */
-@DisplayName("Entity should")
 class EntityTest {
 
-    @DisplayName("perform equals and hashcode methods properly.")
-    @Test
-    void testEntitiesEquals() {
-        String username = "oleg@mail.ua";
-        String password = "qwerty1234";
-        String token = UUID.randomUUID().toString();
+    @Nested
+    @DisplayName("TodoList should")
+    class TodoListTest {
 
-        String todoListId = UUID.randomUUID().toString();
-        String userId = UUID.randomUUID().toString();
+        @Test
+        @DisplayName("perform equals and hashcode methods properly.")
+        void testEqualsAndHashCode() {
+            String todoListId = UUID.randomUUID().toString();
+            String userId = UUID.randomUUID().toString();
 
-        new EqualsTester().addEqualityGroup(new Password(password),
-                new Password(password))
-                .addEqualityGroup(
-                        new Token(token),
-                        new Token(token))
-                .addEqualityGroup(
-                        new Username(username),
-                        new Username(username))
-                .addEqualityGroup(
-                        new TodoList.TodoListBuilder()
-                                .setTodoListId(new TodoListId(todoListId))
-                                .setOwner(new UserId(userId))
-                                .build(),
-                        new TodoList.TodoListBuilder()
-                                .setTodoListId(new TodoListId(todoListId))
-                                .setOwner(new UserId(userId))
-                                .build())
-                .testEquals();
 
-        String taskId = UUID.randomUUID().toString();
-        String description = "description";
-        Date creationDate = new Date();
-        Date lastUpdateDate = new Date();
+            new EqualsTester()
+                    .addEqualityGroup(
+                            new TodoList.TodoListBuilder()
+                                    .setTodoListId(new TodoListId(todoListId))
+                                    .setOwner(new UserId(userId))
+                                    .build(),
+                            new TodoList.TodoListBuilder()
+                                    .setTodoListId(new TodoListId(todoListId))
+                                    .setOwner(new UserId(userId))
+                                    .build())
+                    .testEquals();
+        }
+    }
 
-        new EqualsTester()
-                .addEqualityGroup(
-                        new Task.TaskBuilder()
-                                .setTaskId(new TaskId(taskId))
-                                .setTodoListId(new TodoListId(todoListId))
-                                .setDescription(description)
-                                .setCreationDate(creationDate)
-                                .completed()
-                                .setLastUpdateDate(lastUpdateDate)
-                                .build(),
-                        new Task.TaskBuilder()
-                                .setTaskId(new TaskId(taskId))
-                                .setTodoListId(new TodoListId(todoListId))
-                                .setDescription(description)
-                                .setCreationDate(creationDate)
-                                .completed()
-                                .setLastUpdateDate(lastUpdateDate)
-                                .build()
-                );
+    @Nested
+    @DisplayName("Token should")
+    class TokenTest {
+
+        @Test
+        @DisplayName("perform equals and hashcode methods properly.")
+        void testEqualsAndHashCode() {
+            String token = UUID.randomUUID().toString();
+
+            new EqualsTester()
+                    .addEqualityGroup(
+                            new Token(token),
+                            new Token(token))
+                    .testEquals();
+        }
+    }
+
+    @Nested
+    @DisplayName("Password should")
+    class PasswordTest {
+
+        @Test
+        @DisplayName("perform equals and hashcode methods properly.")
+        void testEqualsAndHashCode() {
+            String password = "qwerty1234";
+
+            new EqualsTester()
+                    .addEqualityGroup(
+                            new Password(password),
+                            new Password(password))
+                    .testEquals();
+        }
+    }
+
+    @Nested
+    @DisplayName("Username should")
+    class UsernameTest {
+
+        @Test
+        @DisplayName("perform equals and hashcode methods properly.")
+        void testEqualsAndHashCode() {
+            String username = "oleg@mail.ua";
+
+            new EqualsTester()
+                    .addEqualityGroup(
+                            new Username(username),
+                            new Username(username))
+                    .testEquals();
+        }
     }
 }
