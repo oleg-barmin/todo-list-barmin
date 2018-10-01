45f999a44933ee9fba3cb3890a990933ecdf6844
Oleg <olegbarmin@gmail.com>
2018-10-01 19:19:48 +0300

    Repackaging completed.

--- rollup.config.js
+++ rollup.config.js
@@ -1,38 +1,38 @@
 export default [
     {
-        input: 'test/model/todo-test.js',
+        input: 'js/test/model/todo-test.js',
         output: {
-            file: 'test/model/todo-test-bundle.js',
+            file: 'js/test/model/todo-test-bundle.js',
             format: 'umd',
         },
     },
     {
-        input: 'test/event/even-bus-test.js',
+        input: 'js/test/event/even-bus-test.js',
         output: {
-            file: 'test/event/even-bus-test-bundle.js',
+            file: 'js/test/event/even-bus-test-bundle.js',
             format: 'umd',
         },
     },
     {
-        input: 'test/controller-test.js',
+        input: 'js/test/controller-test.js',
         output: {
-            file: 'test/controller-test-bundle.js',
+            file: 'js/test/controller-test-bundle.js',
             format: 'umd',
         },
     },
     {
-        input: 'src/todo-list-app.js',
+        input: 'js/src/todo-list-app.js',
         output: {
             name: "bundle",
-            file: 'public/js/bundle.js',
+            file: 'web/src/main/resources/js/bundle.js',
             format: 'umd',
         }
     },
     {
-        input: 'test/view/todo-widget-test.js',
+        input: 'js/test/view/todo-widget-test.js',
         output: {
             name: "bundle",
-            file: 'test/view/todo-widget-test-bundle.js',
+            file: 'js/test/view/todo-widget-test-bundle.js',
             format: 'umd',
         }
     }
