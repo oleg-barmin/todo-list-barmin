82e148d5c53da3bbce44788031862ae26dd0124b
Oleg <olegbarmin@gmail.com>
2018-10-02 14:36:58 +0300

    Add package-info to entity and operation packages.

---
+++ model/src/main/java/org/javaclasses/todo/model/entity/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Contains all entities used in to-do list application.
+ */
+@CheckReturnValue
+@ParametersAreNonnullByDefault
+package org.javaclasses.todo.model.entity;
+
+import javax.annotation.CheckReturnValue;
+import javax.annotation.ParametersAreNonnullByDefault;
---
+++ model/src/main/java/org/javaclasses/todo/model/operation/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Contains all operation which are used by {@code TodoService}.
+ */
+@CheckReturnValue
+@ParametersAreNonnullByDefault
+package org.javaclasses.todo.model.operation;
+
+import javax.annotation.CheckReturnValue;
+import javax.annotation.ParametersAreNonnullByDefault;
