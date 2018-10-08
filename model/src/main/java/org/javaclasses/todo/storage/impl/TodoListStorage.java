c89fc1a3d2561ea97c84ccbce5e87537c298c9a9
Oleg <olegbarmin@gmail.com>
2018-10-08 12:13:58 +0300

    Add method which finds users to-do lists to TodoListStorage.

--- model/src/main/java/org/javaclasses/todo/storage/impl/TodoListStorage.java
+++ model/src/main/java/org/javaclasses/todo/storage/impl/TodoListStorage.java
@@ -3,7 +3,9 @@ package org.javaclasses.todo.storage.impl;
 import com.google.common.annotations.VisibleForTesting;
 import org.javaclasses.todo.model.entity.TodoList;
 import org.javaclasses.todo.model.entity.TodoListId;
+import org.javaclasses.todo.model.entity.UserId;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -19,5 +21,17 @@ public class TodoListStorage extends InMemoryStorage<TodoListId, TodoList> {
     @VisibleForTesting
     TodoListStorage(Map<TodoListId, TodoList> map) {
         super(map);
+    }
+
+    /**
+     * Reads all {@link TodoList}s of user with given ID.
+     *
+     * <p>If returned list is empty then user with given ID has no {@code TodoList}s.
+     *
+     * @param userId ID of user whose to-do list is needed
+     * @return list of {@link TodoList}s of user with given ID
+     */
+    List<TodoList> readTodoListsOf(UserId userId) {
+        return findByField("owner", userId);
     }
 }
