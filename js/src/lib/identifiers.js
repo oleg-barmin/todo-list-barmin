8b1b0c86d75fc80db376bd59cf3b7141e5c401bf
Oleg <olegbarmin@gmail.com>
2018-10-08 16:10:04 +0300

    Add TodoListId to js module.

--- js/src/lib/identifiers.js
+++ js/src/lib/identifiers.js
@@ -2,6 +2,8 @@ import {Preconditions} from "./preconditions";
 
 /**
  * Used to identify tasks.
+ *
+ * @author Oleg Barmin
  */
 export class TaskId {
 
@@ -18,7 +20,7 @@ export class TaskId {
     /**
      * Compares `TaskId` objects by stored ID.
      *
-     * @param taskId `TaskId` to compare with
+     * @param {TaskId} taskId `TaskId` to compare with
      *
      * @throws TypeError if given `taskId` is not TaskId class instance
      *
@@ -30,5 +32,40 @@ export class TaskId {
         }
 
         return this.id.localeCompare(taskId.id);
+    }
+}
+
+/**
+ * Used to identify `TodoList`s.
+ *
+ * @author Oleg Barmin
+ */
+export class TodoListId {
+
+    /**
+     * Creates `TodoListId` instance.
+     *
+     * @param id ID to store
+     */
+    constructor(id) {
+        Preconditions.checkStringNotEmpty(id, "ID");
+        this.id = id;
+    }
+
+    /**
+     * Compares `TodoListId` objects by stored ID.
+     *
+     * @param {TodoListId} todoListId `TodoListId` to compare with
+     *
+     * @throws TypeError if given `todoListId` is not TodoListId class instance
+     *
+     * @returns {number} result positive, negative or 0 if given task is less, greater or equal to current.
+     */
+    compareTo(todoListId) {
+        if (!(todoListId instanceof TodoListId)) {
+            throw new TypeError("Object of TodoListId was expected");
+        }
+
+        return this.id.localeCompare(todoListId.id);
     }
 }
