a5611ae83632ccba58a6ecbb66aa78c6844c7283
Oleg <olegbarmin@gmail.com>
2018-10-09 11:30:14 +0300

    Add status and lastUpdateDate parameters to Tasks constructor.

--- js/src/model/task.js
+++ js/src/model/task.js
@@ -1,10 +1,13 @@
 "use strict";
+
 /**
  * Task to do.
  *
  * It stores task ID, its description, status (is it completed or not),
  * date of creation and date of last update (if task wasn't updated
  * the date of creation equals to last update date).
+ *
+ * @author Oleg Barmin
  */
 export class Task {
 
@@ -15,13 +18,15 @@ export class Task {
      * @param {string} description description of the task
      * @param {Date} creationDate date when task was created
      *
+     * @param {boolean} [completed=false] status of task (completed or not)
+     * @param {Date} [lastUpdate=creationDate] date of last task update
      */
-    constructor(id, description, creationDate) {
+    constructor(id, description, creationDate, completed = false, lastUpdate = creationDate) {
         this.id = id;
         this.description = description;
-        this.completed = false;
+        this.completed = completed;
         this.creationDate = creationDate;
-        this.lastUpdateDate = creationDate;
+        this.lastUpdateDate = lastUpdate;
     }
 
     toString() {
