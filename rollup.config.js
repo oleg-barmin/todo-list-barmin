a52b74e0d53ec5fe114ea947766957441e7114d8
Oleg <olegbarmin@gmail.com>
2018-10-10 16:52:28 +0300

    Change rollup config according to new tests.

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
 export default [
     {
-        input: 'js/test/model/todo-test.js',
+        input: 'js/test/lib/lib-test.js',
         output: {
-            file: 'js/test/model/todo-test-bundle.js',
+            file: 'js/test/lib/lib-test-bundle.js',
             format: 'umd',
         },
     },
@@ -10,6 +10,13 @@ export default [
         input: 'js/test/event/even-bus-test.js',
         output: {
             file: 'js/test/event/even-bus-test-bundle.js',
+            format: 'umd',
+        },
+    },
+    {
+        input: 'js/test/model/todo-list-test.js',
+        output: {
+            file: 'js/test/model/todo-list-test-bundle.js',
             format: 'umd',
         },
     },
